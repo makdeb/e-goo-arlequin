bec5a3ef19a9c9bac5bd3def86d05b156eaf20a9
Oleg <o-makdeb@yandex.ru>
2012-11-07 20:08:10 +0200

    add counter

--- bonfire/themes/sports/footer.php
+++ bonfire/themes/sports/footer.php
@@ -36,6 +36,24 @@
 
 
 		<div id='last'>
-			<p> Copyright &copy; 2012 	</p>		
+			<p> Copyright &copy; 2012 	</p>
+			<div id='counter'>
+				<!--LiveInternet counter-->
+				<script type="text/javascript">
+				<!--
+					document.write("<a href='http://www.liveinternet.ru/click' "+
+					"target=_blank><img src='//counter.yadro.ru/hit?t25.6;r"+
+					escape(document.referrer)+((typeof(screen)=="undefined")?"":
+					";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+					screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+					";"+Math.random()+
+					"' alt='' title='LiveInternet: показано число посетителей за"+
+					" сегодня' "+
+					"border='0' width='88' height='15'><\/a>")
+				//-->
+				</script>
+				<!--/LiveInternet-->	
+			<!--</div>	-->
+		</div>		
 		</div>
 	</div>	
